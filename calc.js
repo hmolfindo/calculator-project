420d7e1e040ca15c0de09ef5b556819f6b6a4961
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-09-13 12:35:31 +0800

    Initalize variables and operate function

--- calc.js
+++ calc.js
@@ -13,3 +13,24 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1/num2;
 }
+
+var num1 = 0;
+var num2 = 0;
+var operator = '';
+
+function operate(operator, firstNum, secondNum){
+    switch(operator){
+        case "+":
+            return firstNum + secondNum;
+            break
+        case "-":
+            return firstNum - secondNum;
+            break;
+        case "*":
+            return firsNum * secondNum;
+            break;
+        case "/":
+            return firstNum / secondNum;
+            break;
+    }
+}
