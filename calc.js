98bbed2944045ee217274e2adc5bc920fc47891e
Milo Olfindo <142918074+hmolfindo@users.noreply.github.com>
2024-09-16 20:25:42 +0800

    Add feature that clears display on second operand press

--- calc.js
+++ calc.js
@@ -1,65 +1,124 @@
 //Addition
-function add(num1, num2){
-    return num1 + num2;
+function addition(num1, num2){
+    return Number(num1) + Number(num2);
 };
 
 //Subtraction
-function subtract(num1, num2){
+function subtraction(num1, num2){
     return num1 - num2;
 };
 
 //Multiplication
-function multiply(num1, num2){
+function multiplication(num1, num2){
     return num1 * num2;
 }
 
 //Division
-function divide(num1, num2){
+function division(num1, num2){
     return num1/num2;
 }
 
 //Variables to use in executing operations
-var num1 = 0;
-var num2 = 0;
-var operator = '';
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+let isClearDisplay = false;
 
-//Operation to execute upon pressing equals button
+//On equals press
 function operate(operator, firstNum, secondNum){
     switch(operator){
         case "+":
-            return firstNum + secondNum;
+            return addition(firstNum, secondNum);
             break;
         case "-":
-            return firstNum - secondNum;
+            return subtraction(firstNum, secondNum);
             break;
         case "*":
-            return firstNum * secondNum;
+            return multiplication(firstNum, secondNum);
             break;
         case "/":
-            return firstNum / secondNum;
+            return division(firstNum, secondNum);
             break;
     }
 };
 
+
+
 const display = document.getElementById("display-area");
 const numKeys = document.querySelectorAll(".num");
+
+function clearDisplay(){
+    display.textContent = "";
+};
 
 //Add event listener to all number buttons
 numKeys.forEach(function(btn){
     btn.addEventListener("click", function(event){
-    display.textContent += this.value;
-    display.value = display.textContent;
+        if(isClearDisplay){
+            clearDisplay();
+            isClearDisplay = false;
+        }
+        display.textContent += this.value;
+        display.value = display.textContent;
     })
+        
 });
+
+/* ON NUMBER AND OPERATOR PRESS
+Assign value of num1 and operator based on input
+ON NUMBER PRESS AFTER OPERATOR
+Clear display and listen for num2
+ON EQUALS PRESS
+Assign value to num2 and execute operate function
+Display result */
+
+//Add event listener for operators
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+
+add.addEventListener("click", () => {
+    num1 = display.value;
+    operator = "+";
+    isClearDisplay = true;
+});
+subtract.addEventListener("click", () => {
+    num1 = display.value;
+    operator = "-";
+    isClearDisplay = true;
+});
+multiply.addEventListener("click", () => {
+    num1 = display.value;
+    operator = "*";
+    isClearDisplay = true;
+});
+divide.addEventListener("click", () => {
+    num1 = display.value;
+    operator = "/";
+    isClearDisplay = true;
+})
 
 //Add event listener for other buttons
-const equals = document.getElementById("equals");
-const del = document.getElementById("delete");
-const clear = document.getElementById("clear");
+const equalsBtn = document.getElementById("equals");
+const delBtn = document.getElementById("delete");
+const clearBtn = document.getElementById("clear");
 
-clear.addEventListener("click", () => {
-    display.textContent = "";
+equalsBtn.addEventListener("click", () => {
+    num2 = display.value;
+    let result = operate(operator, num1, num2);
+    display.textContent = Number(result);
+    isClearDisplay = true;
+    /* num1 = 0;
+    num2 = 0;
+    operator = ""; */
 });
-del.addEventListener("click", () => {
-    display.textContent = display.textContent.slice(0, display.textContent.length -1);
+delBtn.addEventListener("click", () => {
+    display.textContent = display.textContent.slice(0, display.textContent.length-1);
+});
+clearBtn.addEventListener("click", () => {
+    clearDisplay();
+    num1 = 0;
+    num2 = 0;
+    operator = "";
 });
